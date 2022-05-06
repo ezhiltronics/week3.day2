52698134fb09d90a3d37d6cb3bef095735dd1345
ezhiltronics <103839979+ezhiltronics@users.noreply.github.com>
2022-05-06 17:19:17 +0530

    Add files via upload

---
+++ FindIntersection.java
@@ -0,0 +1,21 @@
+package week3.day2;
+
+public class FindIntersection {
+	
+	public static void main(String[] args) {
+		int[] arr1= {1,3,6,8,9};
+		int[] arr2= {3,6,67,9,34};
+		
+		for(int i=0;i<arr1.length;i++)
+		{
+			for(int j=0;j<arr2.length;j++)
+			{
+				if(arr1[i]==arr2[j])
+				{
+					System.out.println(arr1[i]);
+				}
+			}
+		}
+
+}
+}
